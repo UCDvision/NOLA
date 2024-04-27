e009fdcc715f3bad5829b8cb72269eea5e7b4e7b
Soroush <soroush@vision11.idav.ucdavis.edu>
2024-04-27 12:00:06 -0400

    llama and gpt finetuning

---
+++ gpt/scripts/finetune_gpt2l_e2e_qv_nola.sh
@@ -0,0 +1,70 @@
+PEFT="NOLA"
+NUM_BASIS=256 
+RANK="8" 
+LORA_ALPHA=2 
+LR=0.01
+
+python -m torch.distributed.launch --master_port $1 --nproc_per_node=1 src/gpt2_ft.py \
+    --train_data ./data/e2e/train.jsonl \
+    --valid_data ./data/e2e/valid.jsonl \
+    --log_interval 500 \
+    --eval_interval 25000 \
+    --train_batch_size 4 \
+    --grad_acc 4 \
+    --valid_batch_size 4 \
+    --seq_len 512 \
+    --model_card gpt2.lg \
+    --init_checkpoint ./pretrained_checkpoints/gpt2-large-pytorch_model.bin \
+    --platform local \
+    --clip 0.0 \
+    --weight_decay 0.0 \
+    --correct_bias \
+    --adam_beta2 0.999 \
+    --scheduler linear \
+    --warmup_step 500 \
+    --max_epoch 5 \
+    --save_interval 50000 \
+    --label_smooth 0.1 \
+    --lora_qv \
+    --use_nola \
+    --nola_num_basis $NUM_BASIS \
+    --lora_rank $RANK \
+    --lora_alpha $LORA_ALPHA \
+    --lr $LR \
+    --work_dir /nfs_share3/code/soroush/checkpoints/trained_models/GPT2_L/e2e_qv_${PEFT}_lr${LR}_rank${RANK}_alpha${LORA_ALPHA}_nbasis${NUM_BASIS} \
+    --random_seed 110
+    
+    
+    
+python -m torch.distributed.launch --master_port $1 --nproc_per_node=1 src/gpt2_beam.py \
+    --data ./data/e2e/test.jsonl \
+    --batch_size 1 \
+    --seq_len 512 \
+    --eval_len 64 \
+    --model_card gpt2.lg \
+    --init_checkpoint /nfs_share3/code/soroush/checkpoints/trained_models/GPT2_L/e2e_qv_${PEFT}_lr${LR}_rank${RANK}_alpha${LORA_ALPHA}_nbasis${NUM_BASIS}/model.52580.pt \
+    --platform local \
+    --lora_qv \
+    --use_nola \
+    --nola_num_basis $NUM_BASIS \
+    --lora_rank $RANK \
+    --lora_alpha $LORA_ALPHA \
+    --beam 10 \
+    --length_penalty 0.8 \
+    --no_repeat_ngram_size 4 \
+    --repetition_penalty 1.0 \
+    --eos_token_id 628 \
+    --work_dir /nfs_share3/code/soroush/checkpoints/trained_models/GPT2_L/e2e_qv_${PEFT}_lr${LR}_rank${RANK}_alpha${LORA_ALPHA}_nbasis${NUM_BASIS} \
+    --output_file predict.52580.b10p08r4.jsonl
+    
+    
+python src/gpt2_decode.py     --vocab ./vocab \
+--sample_file /nfs_share3/code/soroush/checkpoints/trained_models/GPT2_L/e2e_qv_${PEFT}_lr${LR}_rank${RANK}_alpha${LORA_ALPHA}_nbasis${NUM_BASIS}/predict.52580.b10p08r4.jsonl \
+--input_file ./data/e2e/test_formatted.jsonl \
+--output_ref_file e2e_ref.txt \
+--output_pred_file e2e_pred_${PEFT}${LR}${RANK}${LORA_ALPHA}${NUM_BASIS}.txt
+
+python eval/e2e/measure_scores.py e2e_ref.txt e2e_pred_${PEFT}${LR}${RANK}${LORA_ALPHA}${NUM_BASIS}.txt -p
+
+
+
---
+++ llama/scripts/finetune_llama2_alpaca_13b_nola.sh
@@ -0,0 +1,47 @@
+python qlora.py \
+    --model_name_or_path /nfs_share3/code/soroush/LLM/Llama-2-13b-hf \
+    --output_dir ./output/llama-2-guanaco-7b \
+    --logging_steps 5 \
+    --save_strategy steps \
+    --data_seed 42 \
+    --save_steps 500 \
+    --save_total_limit 40 \
+    --evaluation_strategy steps \
+    --eval_dataset_size 1024 \
+    --max_eval_samples 1000 \
+    --per_device_eval_batch_size 1 \
+    --max_new_tokens 32 \
+    --dataloader_num_workers 8 \
+    --group_by_length \
+    --logging_strategy steps \
+    --remove_unused_columns False \
+    --do_train \
+    --do_eval \
+    --do_mmlu_eval \
+    --mmlu_split 'test'\
+    --use_nola \
+    --lora_r 16 \
+    --nola_num_basis 512 \
+    --lora_alpha 4 \
+    --quant_type nf4 \
+    --bf16 \
+    --bits 4 \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type constant \
+    --gradient_checkpointing \
+    --dataset alpaca \
+    --source_max_len 384 \
+    --target_max_len 512 \
+    --per_device_train_batch_size 16 \
+    --gradient_accumulation_steps 16 \
+    --max_steps 200 \
+    --eval_steps 200 \
+    --learning_rate 0.001 \
+    --adam_beta2 0.999 \
+    --max_grad_norm 0.9 \
+    --lora_dropout 0.0 \
+    --weight_decay 0.0 \
+    --seed 0 \
+
+
+
